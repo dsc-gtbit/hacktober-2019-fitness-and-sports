8c1418ba7de8bb494b9e03587bf8370bae7389cc
Pawan Kumar <pk9910765616@gmail.com>
2019-10-25 16:22:04 +0530

    Update ChestInjury.kt

--- shinigami/app/src/main/java/com/example/fitnessandsports/injury/ChestInjury.kt
+++ shinigami/app/src/main/java/com/example/fitnessandsports/injury/ChestInjury.kt
@@ -21,12 +21,10 @@ import kotlin.collections.ArrayList
  * A simple [Fragment] subclass.
  */
 class ChestInjury : BaseFragment() {
-
-
+    
     var horizontal_recycler_view: RecyclerView? = null
     var horizontalAdapter: HorizontalAdapter? = null
     private var data: List<Data>? = null
-
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_chest_injury, container, false)
@@ -34,26 +32,19 @@ class ChestInjury : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        
         data = fill_with_data()
-
         horizontalAdapter = HorizontalAdapter(data!!, baseActivity)
         val horizontalLayoutManager =
             LinearLayoutManager(baseActivity, LinearLayoutManager.HORIZONTAL, false)
         mRecyclerview!!.layoutManager = horizontalLayoutManager
         mRecyclerview!!.adapter = horizontalAdapter
-
-
     }
 
     fun fill_with_data(): List<Data> {
-
         val data = ArrayList<Data>()!!
-
         data.add(Data(R.drawable.neck))
         data.add(Data(R.drawable.shoulder))
-
-
         return data
     }
 
@@ -61,41 +52,27 @@ class ChestInjury : BaseFragment() {
         RecyclerView.Adapter<HorizontalAdapter.MyViewHolder>() {
 
         private var horizontalList = Collections.emptyList<Data>()
-
         init {
             this.horizontalList = horizontalList
         }
-
         inner class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-
             internal var imageView: ImageView
-
             init {
                 imageView = view.findViewById(R.id.mImageView) as ImageView
             }
         }
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-            val itemView =
-                LayoutInflater.from(parent.context).inflate(R.layout.view_holder, parent, false)
-
+            val itemView = LayoutInflater.from(parent.context).inflate(R.layout.view_holder, parent, false)
             return MyViewHolder(itemView)
         }
 
         override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-
             holder.imageView.setImageResource(horizontalList.get(position).imageId)
-
-
         }
 
         override fun getItemCount(): Int {
             return horizontalList.size
         }
     }
-
-
-
-
 }
-
